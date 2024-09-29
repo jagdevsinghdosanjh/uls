560661e5e905b13086788d38a8d54a16c45081bd
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2024-09-29 21:10:57 +0530

    Logeed in TDate added

--- home.php
+++ home.php
@@ -12,6 +12,11 @@ if (!isset($_SESSION['username'])) {
 </head>
 <body>
     <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+    Successfully Logged in on:<?php
+// Return current date from the remote server
+$today = date("d/m/Y");
+echo $today;
+?>
     <p><a href="logout.php">Logout</a></p>
 </body>
 </html>
