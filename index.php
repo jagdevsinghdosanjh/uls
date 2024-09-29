b7c725de3fe1a224ca04dd2268f4030282fe858a
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2024-09-29 21:01:32 +0530

    User Login Management Successfull

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="authenticate.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+        <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <a href="register.php">Register here</a></p>
+</body>
+</html>
